5f760f10a6c24d9349f5f464e868838da427081b
eMarket Design <info@emarketdesign.com>
2017-04-09 13:40:06 -0400

    enabled small-menu.js and some css to style the menu button

--- functions.php
+++ functions.php
@@ -440,8 +440,8 @@ function wpas_scripts_styles() {
 	}
 
 	// Include this script to envoke a button toggle for the main navigation menu on small screens
-	//wp_register_script( 'small-menu', trailingslashit( get_template_directory_uri() ) . 'js/small-menu.js', array( 'jquery' ), '20130130', true );
-	//wp_enqueue_script( 'small-menu' );
+wp_register_script( 'small-menu', trailingslashit( get_template_directory_uri() ) . 'js/small-menu.js', array( 'jquery' ), '20130130', true );
+wp_enqueue_script( 'small-menu' );
 
 }
 add_action( 'wp_enqueue_scripts', 'wpas_scripts_styles' );
